900bcb0a1c73926e4a2d7a478bb2ffbf024fcc59
Mike Delano <mike@mediasilo.com>
2017-07-19 11:41:06 -0400

    x

--- phantomjs-script.js
+++ phantomjs-script.js
@@ -1,5 +1,13 @@
 console.log('start phantom');
+
+var page = require('webpage').create();
+    var url = 'https://mediasilo.github.io/grauman/test/safestream-mixpanel.html';
+    console.log('loading ' + url);
+    page.open(url, function (status) {
+        console.log("Page loaded");
+    });
+
 setTimeout(function() {
-    console.log('Boo from phantom');
+    console.log('Completed test');
     phantom.exit();
-}, 1000);
+}, 400000);
